cb0266f469b2f7bed388fde32a5837932b17fe57
Sophie Cheng <sophiecheng181@gmail.com>
2023-07-28 09:18:41 -0400

    Initial Commit

---
+++ Questioff/QuestioffApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestioffApp.swift
+//  Questioff
+//
+//  Created by Scholar on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestioffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
